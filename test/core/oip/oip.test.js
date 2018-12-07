1a71e5589b9ed0edf24a60dcf42f0254de9f86ca
orpheus <ryanachacon@gmail.com>
2018-12-07 14:56:36 -0800

    test publishing

--- test/core/oip/oip.test.js
+++ test/core/oip/oip.test.js
@@ -3,7 +3,8 @@ import {flo_testnet} from '../../../src/config'
 import {isValidWIF} from '../../../src/util/btc'
 import {OIP} from '../../../src'
 import floTx from 'fcoin/lib/primitives/tx'
-import testArtifact from '../../../examples/exampleArtifact'
+import Network from 'fcoin/lib/protocol/network'
+import {artifactSmall, artifactLarge} from '../../../examples/exampleArtifact'
 import Artifact from "../../../src/modules/records/artifact/artifact";
 
 const network = flo_testnet.network
@@ -70,20 +71,62 @@ describe(`OIP`, () => {
 		})
 	})
 	describe('Publishing', () => {
+		it('publish test artifact', async () => {
+			let oip = new OIP(wif, "testnet")
+			expect(artifactSmall).toBeInstanceOf(Artifact)
+			let txid = await oip.publish(artifactSmall)
+			expect(txid).toBeDefined()
+			expect(typeof txid).toEqual('string')
+			// console.log(txid)
+		})
+		it('publish multiparted artifact', async (done) => {
+			// let str = '1d6392c44629a1fc3eafab4b564a003084e9afad055b5cbdb8fc8c1d3f042d1d,H9dqFw5Pd//qwHeEQA+ENifGvvs/0X1sLUXLQKj2L5qdI/BIJMBX2w3TKETHeNg3MMhA1i3PYVT2FnC8y/BxvUM=,FLZXRaHzVPxJJfaoM32CWT4GZHuj2rx63k,'
+			// console.log(str.length)
+			let oip = new OIP(wif, "testnet")
+			// oip.deleteHistory()
+			expect(artifactLarge).toBeInstanceOf(Artifact)
+			let txids = await oip.publish(artifactLarge)
+			expect(txids).toBeDefined()
+			expect(Array.isArray(txids)).toBeTruthy()
+			// console.log('final return: ', txids)
+			done()
+		}, 250 * 100 * 100)
+	})
+	describe('Chain API', () => {
+		it.skip('test insight api update time', async (done) => {
+			let oip = new OIP(wif, 'testnet')
+
+			for (let i = 0; i < 5; i++) {
+				let txid = await oip.sendToFloChain('11:22')
+				console.log(txid)
+				let start = Date.now(), finish;
+				// console.log(utxo)
+				const slam = async () => {
+					// console.log('slam')
+					let utxo
+					try {
+						utxo = await oip.getUTXO()
+
+					} catch(err) {console.log('err', err)}
+					for (let u of utxo) {
+						if (u.txid === txid) {
+							finish = Date.now()
+						}
+					}
+				}
+				while (!finish) {
+					await slam()
+				}
+				console.log(finish - start)
+			}
+			done()
+		}, 250 * 100 * 100)
 		it('build and broadcast TX hex | sendToFloChain', async () => {
 			let pub = new OIP(wif, "testnet")
 			let txid = await pub.sendToFloChain(`RC`)
 			expect(typeof txid === 'string').toBeTruthy()
 			// console.log(txid)
 		})
-		it('publish test artifact', async () => {
-			let oip = new OIP(wif, "testnet")
-			expect(testArtifact).toBeInstanceOf(Artifact)
-			let txid = await oip.publish(testArtifact)
-			 console.log(txid)
-		})
-	})
-	describe('Chain Functions', () => {
 		it('Create and Send a FLO TX', async () => {
 			let pub = new OIP(wif,  "testnet")
 			let output = {
@@ -91,13 +134,13 @@ describe(`OIP`, () => {
 				value: Math.floor(0.0001 * flo_testnet.satPerCoin)
 			}
 			let txid = await pub.createAndSendFloTx(output, "to testnet")
-			console.log(txid)
+			// console.log(txid)
 			expect(txid).toBeDefined()
 			expect(typeof txid === 'string').toBeTruthy()
 		})
 	})
 	describe('Spent Transactions', () => {
-		it('add spent transaction and remove spent transaction from utxo', async () => {
+		it('add spent remove spent transaction from utxo', async () => {
 			let oip = new OIP(wif, "testnet")
 			let utxo = await oip.getUTXO()
 			// console.log(utxo)
@@ -110,6 +153,42 @@ describe(`OIP`, () => {
 			}
 			let spentTx = oip.getSpentTransactions()[oip.getSpentTransactions().length-1]
 			expect(txid).toEqual(spentTx)
+		})
+		it('loop through history', () => {
+			let oip = new OIP(wif, "testnet")
+			// console.log(oip.ECPair)
+			// const p2pkh = bitcoin.payments.p2pkh({pubkey: ECPair.publicKey, network}).address
+			// console.log(p2pkh)
+
+			let unspents = []
+			for (let txObj of oip.history) {
+				let match = false
+				for (let tx of oip.getSpentTransactions()) {
+					for (let txid in txObj) {
+						if (txid === tx) {
+							match = true
+						}
+					}
+				}
+				if (!match) {
+					unspents.push(txObj)
+				}
+			}
+
+			for (let txObj of unspents) {
+				let ftx;
+				for (let tx in txObj) {
+					ftx = floTx.fromRaw(txObj[tx], 'hex')
+				}
+				//grab output addresses from ftx and convert them to testnet addresses
+
+				// console.log(ftx)
+				// let [addr] = ftx.getOutputAddresses()
+				// addr = addr.toBase58()
+				// let addrBuffer = Buffer.from(addr,  'base58')
+				// console.log(addrBuffer)
+				// console.log(addr.toBase58(network))
+			}
 		})
 	})
 	describe('Local Storage', () => {
