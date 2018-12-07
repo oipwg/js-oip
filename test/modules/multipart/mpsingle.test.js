5d7b4f0ed8febd08f90e00abb19d2a0c61ce52c5
orpheus <ryanachacon@gmail.com>
2018-12-07 08:37:53 -0800

    update/fix mpSingle test

--- test/modules/multipart/mpsingle.test.js
+++ test/modules/multipart/mpsingle.test.js
@@ -58,21 +58,19 @@ describe("MPSingle", () => {
 				`4-6-FLZXRaHzVPxJJfaoM32CWT4GZHuj2rx63k-8c204c5f39-${mps.getData()}`
 			)
 		})
-		it('from Oipindex', async () => {
+		it('from the DaemonApi', async () => {
 			let mp = await index.getMultipart('1d6392c44629a1fc3eafab4b564a003084e9afad055b5cbdb8fc8c1d3f042d1d')
 			expect(mp.success).toBeTruthy()
 			let mps = new MPSingle(mp.multipart)
 			expect(mps.isValid().success).toBeTruthy()
-			expect(mps.isStale()).toBeTruthy()
-			expect(mps.isComplete()).toBeFalsy()
-			expect(mps.getMeta()).toEqual({
-				"stale": true,
-				"block_hash": "455e5d41a5b9b90bd907d6828dbdcb721d82bdc2738ae8b4a5a54bb3869b02cd",
-				"txid": "1d6392c44629a1fc3eafab4b564a003084e9afad055b5cbdb8fc8c1d3f042d1d",
-				"block": 2950932,
-				"time": 1536431891,
-				"complete": false
-			})
+			expect(mps.getMeta()).toEqual({ complete: true,
+				stale: false,
+				time: 1536431891,
+				txid: '1d6392c44629a1fc3eafab4b564a003084e9afad055b5cbdb8fc8c1d3f042d1d',
+				block: 2950932,
+				block_hash: '455e5d41a5b9b90bd907d6828dbdcb721d82bdc2738ae8b4a5a54bb3869b02cd',
+				assembled: '{"oip-041":{"artifact":{"type":"Audio-Album","info":{"extraInfo":{"artist":"Milt Jordan","genre":"Country & Folk","company":"Vintage Heart Records"},"title":"Angels Get the Blues","year":2010,"description":"\\"Original Release Date: December 1, 2010\\\\n\\\\nTotal Length: 20:43\\\\nASIN: B005COCRBO\\\\n\\\\nCopyright: 2010 Milt Jordan\\""},"storage":{"network":"IPFS","files":[{"fname":"1 - Angels Get the Blues.aac","fsize":4861672,"dname":"Angels Get the Blues","sugBuy":1000,"sugPlay":10,"type":"Audio","subtype":"Album Track","duration":306},{"fname":"2 - How I Want My Heaven.aac","fsize":3241607,"dname":"How I Want My Heaven","sugBuy":1000,"sugPlay":10,"type":"Audio","subtype":"Album Track","duration":204},{"fname":"3 - Honey Gives Me Money.aac","fsize":3169998,"dname":"Honey Gives Me Money","sugBuy":1000,"sugPlay":10,"type":"Audio","subtype":"Album Track","duration":199},{"fname":"4 - Never Saw It Coming.aac","fsize":2580019,"dname":"Never Saw It Coming","sugBuy":1000,"sugPlay":10,"type":"Audio","subtype":"Album Track","duration":162},{"fname":"5 - Ride With Joe.aac","fsize":3490473,"dname":"Ride With Joe","sugBuy":1000,"sugPlay":10,"type":"Audio","subtype":"Album Track","duration":219},{"fname":"6 - Hot For You Baby.aac","fsize":2436918,"dname":"Hot For You Baby","sugBuy":1000,"sugPlay":10,"type":"Audio","subtype":"Album Track","duration":153},{"fname":"Vanishing Breed.aac","fsize":4261627,"dname":"Vanishing Breed","sugBuy":1000,"sugPlay":10,"type":"Audio","subtype":"Single Track","duration":268},{"fname":"miltjordan-vanishingbreed.jpg","fsize":40451,"type":"Image","subtype":"album-art"},{"fname":"miltjordan-angelsgettheblues.jpg","fsize":54648,"type":"Image","subtype":"cover"}],"location":"QmWmth4ES4ZH9Wgz6Z7S7dRFF8MzJVGgDhit5KzH5uCvZz"},"payment":{"fiat":"USD","scale":"1000:1","maxdisc":30,"promoter":15,"retailer":15,"sugTip":[],"addresses":[{"token":"BTC","address":"1LqBGSKuX5yYUonjxT5qGfpUsXKYYWeabA"},{"token":"FLO","address":"FTogNNXik7eiHZw5uN2KMe4cvcr7GCEjbZ"},{"token":"LTC","address":"LUWPbpM43E2p7ZSh8cyTBEkvpHmr3cB8Ez"}]},"timestamp":1536429312,"publisher":"FLZXRaHzVPxJJfaoM32CWT4GZHuj2rx63k"},"signature":"IGFvZmnnB7g2Q02zzzKwrgNelhUJv8D69J/HTa+IqHe3IWI/RaT3PLzKjfjeHWSjKC6PjF3EVKXAdibq+5DbYr4="}}',
+				tx: undefined })
 		})
 	})
 	describe('Signing', () => {
