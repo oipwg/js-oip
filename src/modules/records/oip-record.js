20a566917816284d4b2213ad503d8dc295cb6e63
orpheus <ryanachacon@gmail.com>
2018-12-07 08:21:32 -0800

    add super methods for signing and serializing on OIPRecord

--- src/modules/records/oip-record.js
+++ src/modules/records/oip-record.js
@@ -1,5 +1,57 @@
-export default class OIPRecord {
+import {sign} from "bitcoinjs-message";
+import bitcoin from "bitcoinjs-lib";
+
+class OIPRecord {
 	constructor() {
 
 	}
+
+	signSelf(ECPair) {
+		if (!ECPair) {
+			return {success: false, error: 'Must provide ECPair'}
+		}
+
+		const p2pkh = bitcoin.payments.p2pkh({pubkey: ECPair.publicKey, network: ECPair.network}).address
+		this.setPubAddress(p2pkh)
+
+		let preimage = this.create_preimage()
+
+		let privateKeyBuffer = ECPair.privateKey;
+
+		let compressed = ECPair.compressed || true;
+
+		let signature_buffer
+		try {
+			signature_buffer = sign(preimage, privateKeyBuffer, compressed, ECPair.network.messagePrefix)
+		} catch (e) {
+			return {success: false, error: e}
+		}
+
+		let signature = signature_buffer.toString('base64')
+		this.setSignature(signature)
+
+		return {success: true, signature}
+
+	}
+
+	setSignature(sig) {
+		this.signature = sig
+	}
+	getSignature() {
+		return this.signature
+	}
+	setPubAddress(pubAddress) {
+		this.pubAddress = pubAddress
+	}
+	getPubAddress() {
+		return this.pubAddress
+	}
+	create_preimage() {
+		throw new Error(`Classes that extend OIPRecord must contain a 'create_preimage' method`)
+	}
+	serialize() {
+		throw new Error(`Classes that extend OIPRecord must contain a 'serialize' method`)
+	}
 }
+
+export default OIPRecord
