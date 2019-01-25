22c446e132e37845227c6edd0b3d3a029bdbf1a7
Sky Young <skyoung@mediciventures.com>
2019-01-24 18:21:07 -0700

    RPC wallet authenticates!

---
+++ test/modules/wallets/RPCWallet.test.js
@@ -0,0 +1,28 @@
+import RPCWallet from '../../../src/modules/wallets/RPCWallet'
+
+let pubAddress = "pubaddress"
+let wif = "wif"
+
+let wallet = new RPCWallet({
+	publicAddress: pubAddress,
+	wif,
+	network: "testnet",
+	rpc: {
+		port: 17313,
+		host: "127.0.0.1",
+		username: "username",
+		password: "password"
+	}
+})
+
+test("Filler test (ignore me)", () => {
+	let x = true
+
+	expect(x).toBe(true)
+})
+
+test.skip("Can initialize", async () => {
+	let init = await wallet.initialize()
+
+	expect(init).toBe(true)
+})
