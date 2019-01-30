52670e8e2c38ec240ccdc0ddc6f11642d9cf3f70
Sky Young <skyoung@mediciventures.com>
2019-01-30 08:31:21 -0700

    Use most recent utxo to chain off of

--- src/modules/wallets/RPCWallet.js
+++ src/modules/wallets/RPCWallet.js
@@ -72,7 +72,7 @@ class RPCWallet {
 
 		let input
 
-		for (let i = (utxos.length - 1); i >= 0; i--) {
+		for (let i = 0; i < utxos.length; i++) {
 			if (utxos[i].amount > 0.0001){
 				input = utxos[i]
 				break
