ca4b1871081b16c615e0ec4d8f7a6cdc441c73a6
Sky Young <skyoung@mediciventures.com>
2019-03-05 12:47:40 -0700

    Add Edit Record base

---
+++ src/modules/records/edit/edit.js
@@ -0,0 +1,65 @@
+import OIPRecord from '../oip-record'
+
+export default class EditRecord extends OIPRecord {
+	constructor() {
+		super()
+
+		// Define the edit information
+		this.edit = {
+			txid: undefined,
+			timestamp: undefined,
+			patch: undefined
+		}
+
+		this.meta = {
+			applied: undefined
+		}
+
+		// Define variables to hold the current latest version record and the updated record
+		this.previousRecordVersion = undefined
+		this.updatedRecordVersion = undefined
+
+		// Var to hold the internal rfc patch
+		this.rfc6902Patch = undefined
+	}
+
+	setUpdatedRecord(updatedRecord) {
+		this.updatedRecordVersion = updatedRecord
+	}
+
+	setPreviousRecord(previousRecord) {
+		this.previousRecordVersion = previousRecord
+	}
+
+	createRFC6902Patch() {
+
+	}
+
+	squashRFC6902Patch() {
+
+	}
+
+	unsquashRFC6902() {
+
+	}
+
+	createPatch() {
+
+	}
+
+	/**
+	 * Get the JSON version of the edit
+	 * @return {[type]} [description]
+	 */
+	toJSON() {
+		
+	}
+
+	fromJSON() {
+		
+	}
+
+	serialize() {
+
+	}
+}
