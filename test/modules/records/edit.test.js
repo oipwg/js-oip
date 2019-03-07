a164c5780439dacf4d38aca16161e06579fe9d1e
Sky Young <skyoung@mediciventures.com>
2019-03-07 14:08:42 -0700

    standard --fix edit.test.js

--- test/modules/records/edit.test.js
+++ test/modules/records/edit.test.js
@@ -1,90 +1,90 @@
 import { Artifact as Record } from '../../../src/modules/records/artifact'
-import EditRecord from '../../../src/modules/records/edit'
+import { EditRecord } from '../../../src/modules/records/edit'
 
 describe('RFC6902 Tests', () => {
   test('create RFC6902 Patch', () => {
     let edit = new EditRecord()
 
-    let rfc6902Patch = edit.createRFC6902Patch({ field: "before" }, { field: "after" })
+    let rfc6902Patch = edit.createRFC6902Patch({ field: 'before' }, { field: 'after' })
 
-    expect(rfc6902Patch).toEqual([ { op: "replace", path: "/field", value: "after" } ])
+    expect(rfc6902Patch).toEqual([ { op: 'replace', path: '/field', value: 'after' } ])
   })
 })
 
 describe('Squash RFC6902', () => {
   test('squash RFC6902 Patch (single field, single op)', () => {
-     let edit = new EditRecord()
+    let edit = new EditRecord()
 
-    let rfc6902Patch = [ { op: "replace", path: "/field", value: "after" }, { op: "replace", path: "/second", value: "two" } ]
+    let rfc6902Patch = [ { op: 'replace', path: '/field', value: 'after' } ]
 
     let squashedPatch = edit.squashRFC6902Patch(rfc6902Patch)
 
-    expect(squashedPatch).toBe({ replace: { "/field": "after" } })
+    expect(squashedPatch).toEqual({ replace: { '/field': 'after' } })
   })
   test('squash RFC6902 Patch (multiple fields, single op)', () => {
-     let edit = new EditRecord()
+    let edit = new EditRecord()
 
-    let rfc6902Patch = [ 
-      { op: "replace", path: "/field", value: "after" },
-      { op: "replace", path: "/second", value: "two" }
+    let rfc6902Patch = [
+      { op: 'replace', path: '/field', value: 'after' },
+      { op: 'replace', path: '/second', value: 'two' }
     ]
 
     let squashedPatch = edit.squashRFC6902Patch(rfc6902Patch)
 
-    expect(squashedPatch).toBe({ replace: { "/field": "after", "/second": "two" } })
+    expect(squashedPatch).toEqual({ replace: { '/field': 'after', '/second': 'two' } })
   })
   test('squash RFC6902 Patch (single fields, multiple ops)', () => {
-     let edit = new EditRecord()
+    let edit = new EditRecord()
 
-    let rfc6902Patch = [ 
-      { op: "remove", path: "/field" },
-      { op: "replace", path: "/second", value: "two" }
+    let rfc6902Patch = [
+      { op: 'remove', path: '/field' },
+      { op: 'replace', path: '/second', value: 'two' }
     ]
 
     let squashedPatch = edit.squashRFC6902Patch(rfc6902Patch)
 
-    expect(squashedPatch).toBe({ 
-      remove: [ "/field" ], 
-      replace: { "/second": "two" } 
+    expect(squashedPatch).toEqual({
+      remove: [ '/field' ],
+      replace: { '/second': 'two' }
     })
   })
   test('squash RFC6902 Patch (multiple fields, multiple ops)', () => {
     let edit = new EditRecord()
 
-    let rfc6902Patch = [ 
-      { op: "remove", path: "/first" },
-      { op: "remove", path: "/second" },
-      { op: "replace", path: "/third", value: "three" },
-      { op: "replace", path: "/fourth", value: "four" },
-      { op: "replace", path: "/fifth", value: "five" },
-      { op: "add", path: "/sixth", value: "six" },
-      { op: "add", path: "/seventh", value: "seven" },
-      { op: "move", from: "/eigth", path: "/newEightLand" },
-      { op: "copy", from: "/ninth", path: "/nineTopia" },
-      { op: "test", path: "/myField", value: "eightyNine" },
+    let rfc6902Patch = [
+      { op: 'remove', path: '/first' },
+      { op: 'remove', path: '/second' },
+      { op: 'replace', path: '/third', value: 'three' },
+      { op: 'replace', path: '/fourth', value: 'four' },
+      { op: 'replace', path: '/fifth', value: 'five' },
+      { op: 'add', path: '/sixth', value: 'six' },
+      { op: 'add', path: '/seventh', value: 'seven' },
+      { op: 'move', from: '/eigth', path: '/newEightLand' },
+      { op: 'copy', from: '/ninth', path: '/nineTopia' },
+      { op: 'test', path: '/myField', value: 'eightyNine' }
     ]
 
     let squashedPatch = edit.squashRFC6902Patch(rfc6902Patch)
 
-    expect(squashedPatch).toBe({ 
-      remove: [ "/first", "/second" ], 
-      replace: { 
-        "/third": "three",
-        "/fourth": "four",
-        "/fifth": "five"
+    expect(squashedPatch).toEqual({
+      remove: [ '/first', '/second' ],
+      replace: {
+        '/third': 'three',
+        '/fourth': 'four',
+        '/fifth': 'five'
       },
       add: {
-        "/sixth": "six",
-        "/seventh": "seven"
+        '/sixth': 'six',
+        '/seventh': 'seven'
       },
       move: {
-        "/eigth": "/newEightLand"
+        '/eigth': '/newEightLand'
       },
       copy: {
-        "/ninth": "/nineTopia"
+        '/ninth': '/nineTopia'
       },
       test: {
-        "/myField": "eightyNine"
+        '/myField': 'eightyNine'
       }
     })
   })
@@ -93,81 +93,81 @@ describe('Unsquash RFC6902', () => {
   test('unsquash RFC6902 Patch', () => {
     let edit = new EditRecord()
 
-    let squashedPatch = { 
-      remove: [ "/first", "/second" ], 
-      replace: { 
-        "/third": "three",
-        "/fourth": "four",
-        "/fifth": "five"
+    let squashedPatch = {
+      remove: [ '/first', '/second' ],
+      replace: {
+        '/third': 'three',
+        '/fourth': 'four',
+        '/fifth': 'five'
       },
       add: {
-        "/sixth": "six",
-        "/seventh": "seven"
+        '/sixth': 'six',
+        '/seventh': 'seven'
       },
       move: {
-        "/eigth": "/newEightLand"
+        '/eigth': '/newEightLand'
       },
       copy: {
-        "/ninth": "/nineTopia"
+        '/ninth': '/nineTopia'
       },
       test: {
-        "/myField": "eightyNine"
+        '/myField': 'eightyNine'
       }
     }
 
     let rfc6902Patch = edit.unsquashRFC6902Patch(squashedPatch)
 
-    expect(rfc6902Patch).toBe([ 
-      { op: "remove", path: "/first" },
-      { op: "remove", path: "/second" },
-      { op: "replace", path: "/third", value: "three" },
-      { op: "replace", path: "/fourth", value: "four" },
-      { op: "replace", path: "/fifth", value: "five" },
-      { op: "add", path: "/sixth", value: "six" },
-      { op: "add", path: "/seventh", value: "seven" },
-      { op: "move", from: "/eigth", path: "/newEightLand" },
-      { op: "copy", from: "/ninth", path: "/nineTopia" },
-      { op: "test", path: "/myField", value: "eightyNine" },
+    expect(rfc6902Patch).toEqual([
+      { op: 'remove', path: '/first' },
+      { op: 'remove', path: '/second' },
+      { op: 'replace', path: '/third', value: 'three' },
+      { op: 'replace', path: '/fourth', value: 'four' },
+      { op: 'replace', path: '/fifth', value: 'five' },
+      { op: 'add', path: '/sixth', value: 'six' },
+      { op: 'add', path: '/seventh', value: 'seven' },
+      { op: 'move', from: '/eigth', path: '/newEightLand' },
+      { op: 'copy', from: '/ninth', path: '/nineTopia' },
+      { op: 'test', path: '/myField', value: 'eightyNine' }
     ])
   })
 })
 
-describe("Apply Squashed Patch to Record")
+describe('Apply Squashed Patch to Record', () => {
   test('apply Squashed Patch', () => {
-    let original_record = new Record()
+    let originalRecord = new Record()
 
-    original_record.setTXID("testTXID")
-    original_record.setTitle("original title")
+    originalRecord.setTXID('testTXID')
+    originalRecord.setTitle('original title')
 
-    let squashedPatch = { replace: { "/title": "my new thing" } }
+    let squashedPatch = { replace: { '/title': 'my new thing' } }
 
-    let edit = new EditRecord(original_record)
+    let edit = new EditRecord(originalRecord)
 
     // setPatch should apply the patch to the Original Record if there is no "patched record" defined
     edit.setPatch(squashedPatch)
 
-    let modifed_record = edit.getPatchedRecord()
+    let modifedRecord = edit.getPatchedRecord()
 
-    expect(modifed_record.getTitle()).toBe("my new thing")
+    expect(modifedRecord.getTitle()).toBe('my new thing')
   })
 })
 
 describe('Create Squashed Patch from Records', () => {
   test('Create Patch (simple Record)', () => {
-    let original_record = new Record()
+    let originalRecord = new Record()
 
-    original_record.setTXID("testTXID")
-    original_record.setTitle("original title")
+    originalRecord.setTXID('testTXID')
+    originalRecord.setTitle('original title')
 
-    let modified_record = new Record()
-    modifed_record.fromJSON(original_record.toJSON())
+    let modifedRecord = new Record()
+    modifedRecord.fromJSON(originalRecord.toJSON())
 
-    modifed_record.setTitle("my new thing")
+    modifedRecord.setTitle('my new thing')
 
-    let edit = new EditRecord(original_record, modifed_record)
+    let edit = new EditRecord(originalRecord, modifedRecord)
 
     console.log(edit.getPatch())
-    
-    expect(edit.getPatch()).toEqual({ replace: { "/title": "my new thing" } })
+
+    expect(edit.getPatch()).toEqual({ replace: { '/title': 'my new thing' } })
   })
 })
